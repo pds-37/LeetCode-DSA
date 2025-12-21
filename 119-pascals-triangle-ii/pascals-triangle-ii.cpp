da0a0905a0976eb8f3b9ccc7cda982325505880c
Priyanshu tiwari <official.priyanshu.37@gmail.com>
2025-12-22 01:56:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (94.09%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ansRow;
+        long long ans = 1;   // use long long for safety
+        ansRow.push_back(1);
+
+        for (int i = 1; i <= rowIndex; i++) {
+            ans = ans * (rowIndex - i + 1);
+            ans = ans / i;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+};
