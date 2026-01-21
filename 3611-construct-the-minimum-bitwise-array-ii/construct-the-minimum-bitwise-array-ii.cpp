7feda5e077b3d34881391f29b3d1b399948c3659
Priyanshu tiwari <official.priyanshu.37@gmail.com>
2026-01-21 22:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (83.50%) - LeetSync

--- 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
@@ -6,15 +6,13 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             int p = nums[i];
             
-            // Case 1: Even prime (2) cannot be formed by x | (x+1)
+            
             if (p == 2) {
                 ans[i] = -1;
                 continue;
             }
 
-            // Case 2: Odd numbers
-            // Find the position of the first zero bit starting from LSB
-            // This tells us the range of the trailing 1s.
+            
             int bit = 1;
             int temp = p;
             
@@ -23,8 +21,7 @@ public:
                 bit <<= 1;
             }
             
-            // 'bit' is now just above the trailing 1s.
-            // We want to subtract half of 'bit' (the MSB of the trailing 1s).
+            
             ans[i] = p - (bit >> 1);
         }
         return ans;
